55c6d0427ecce3529ca7a4db7ecfd030ca39c260
Example.You <you@example.com>
2022-03-13 22:55:33 +1300

    honeypot

--- honeypot.py
+++ honeypot.py
@@ -8,7 +8,6 @@ app = flask.Flask(__name__)
 
 @app.after_request
 def set_response_headers(response):
-    """ Ensures no cache """
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
